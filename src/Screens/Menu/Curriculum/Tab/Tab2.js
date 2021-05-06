05e18501e5b5b86db8f6f81aa00f2fcf28132075
Giang Tran <giangtd@dcv.vn>
2021-05-06 15:39:56 +0700

    update curriculum

---
+++ src/Screens/Menu/Curriculum/Tab/Tab2.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+const Tab2 = (props) => {
+  return (
+    <View style={{flex: 1}}>
+      <Text>Chuyen nganh</Text>
+    </View>
+  );
+};
+
+export default Tab2;
